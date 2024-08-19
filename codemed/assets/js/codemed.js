3b290e770cc77f24343aa8bdab68878836ff245c
Mark Sim <simyixin@yahoo.com.sg>
2024-08-19 23:02:22 +1000

    buttons and slider updated

--- codemed/assets/js/codemed.js
+++ codemed/assets/js/codemed.js
@@ -1,8 +1,17 @@
 const toggleSwitch = document.getElementById('toggleSwitch');
-
 toggleSwitch.addEventListener('click', function() {
     this.classList.toggle('active');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleSegments = document.getElementById('toggleText');
+    const toggleSections = toggleSegments.querySelectorAll('.toggle_section');
+
+    toggleSegments.addEventListener('click', function() {
+        toggleSections.forEach(section => section.classList.toggle('active'));
+    });
+});
+
 
 const buttons = document.querySelectorAll('.tab-button');
 const contentWrapper = document.getElementById('contentWrapper');
@@ -15,3 +24,5 @@ buttons.forEach((button, index) => {
         // contentWrapper.style.transform = `translateX(-${index * 100}%)`;
     });
 });
+
+
